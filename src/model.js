b7ba1bb4a824e7df362f126eda7de066c9fe8b00
Bill Dollins <bill@geomusings.com>
2021-03-18 15:48:17 -0400

    cleanup

--- src/model.js
+++ src/model.js
@@ -4,13 +4,12 @@
   This file is required. It must export a class with at least one public function called `getData`
 
   Documentation: http://koopjs.github.io/docs/usage/provider */
-//const request = require('request').defaults({ gzip: true, json: true })
+// const request = require('request').defaults({ gzip: true, json: true })
 const config = require('config')
-const { BigQuery } = require('@google-cloud/bigquery');
-const bigquery = new BigQuery();
+const { BigQuery } = require('@google-cloud/bigquery')
+const bigquery = new BigQuery()
 
-
-function Model(koop) { }
+function Model (koop) { }
 
 // Public function to return data from the
 // Return: GeoJSON FeatureCollection
@@ -29,70 +28,68 @@ Model.prototype.getData = async function (req, callback) {
   const table = splitPath[1]
   let spatialCol = null
 
-  //pre-flight checks
-  if (!req.params.layer) callback(new Error("Layer not specified"))
-  if (isNaN(parseInt(req.params.layer, 10))) callback(new Error("Layer must be an integer"))
-  if (!table) callback(new Error('The "id" parameter must be in the form of "dataset.table"')) //thanks Dan O'Neill
+  // pre-flight checks
+  if (!req.params.layer) callback(new Error('Layer not specified'))
+  if (isNaN(parseInt(req.params.layer, 10))) callback(new Error('Layer must be an integer'))
+  if (!table) callback(new Error('The "id" parameter must be in the form of "dataset.table"')) // thanks Dan O'Neill
   if (!req.params.method) callback(new Error('Method not specified'))
-  if (req.params.method.toLowerCase() !== "query") callback(new Error(`Method ${req.params.method} not supported`))
+  if (req.params.method.toLowerCase() !== 'query') callback(new Error(`Method ${req.params.method} not supported`))
 
   const layerNum = parseInt(req.params.layer, 10)
-  let err = null;
-  process.env.GOOGLE_CLOUD_PROJECT = config.gcloud.project;
-  process.env.GOOGLE_APPLICATION_CREDENTIALS = "./gcloud/serviceKey.json";
+  process.env.GOOGLE_CLOUD_PROJECT = config.gcloud.project
+  process.env.GOOGLE_APPLICATION_CREDENTIALS = './gcloud/serviceKey.json'
 
-  async function doQuery() {
+  async function doQuery () {
     // Queries the view to get stops along the route.
-    spatialCol = await getSpatialColumn(dataset, table, layerNum);
-    if (!spatialCol) throw new Error("Specified table has no spatial column")
-    if (spatialCol == "-999") throw new Error("Specified layer does not exist")
+    spatialCol = await getSpatialColumn(dataset, table, layerNum)
+    if (!spatialCol) throw new Error('Specified table has no spatial column')
+    if (spatialCol === '-999') throw new Error('Specified layer does not exist')
     console.log(spatialCol)
-    const query = `SELECT st_asgeojson(${spatialCol})as ${config.gcloud.geometry},  * EXCEPT(${spatialCol}) FROM \`${dataset}.${table}\``;
-    console.log(query);
+    const query = `SELECT st_asgeojson(${spatialCol})as ${config.gcloud.geometry},  * EXCEPT(${spatialCol}) FROM \`${dataset}.${table}\``
+    console.log(query)
     // For all options, see https://cloud.google.com/bigquery/docs/reference/rest/v2/jobs/query
     const options = {
       query: query,
       // Location must match that of the dataset(s) referenced in the query.
-      location: 'US',
-    };
+      location: 'US'
+    }
 
     // Run the query as a job
-    const [job] = await bigquery.createQueryJob(options);
-    console.log(`Job ${job.id}started.`);
+    const [job] = await bigquery.createQueryJob(options)
+    console.log(`Job ${job.id}started.`)
 
     // Wait for the query to finish
-    const [rows] = await job.getQueryResults();
+    const [rows] = await job.getQueryResults()
 
     // return the results
-    return rows;
+    return rows
   }
   // [END bigquery_query]
 
   // translate the response into geojson
-    try {
-      const geojson = translate(await doQuery())
-      // hand off the data to Koop
-      callback(null, geojson)
-    }
-    catch (e) {
-      callback(e)
-    }
+  try {
+    const geojson = translate(await doQuery())
+    // hand off the data to Koop
+    callback(null, geojson)
+  } catch (e) {
+    callback(e)
+  }
 }
 
-function translate(input) {
-  //console.log(input);
+function translate (input) {
+  // console.log(input);
   return {
     type: 'FeatureCollection',
     features: input.map(formatFeature)
   }
 }
 
-function formatFeature(inputFeature) {
+function formatFeature (inputFeature) {
   // get geometry
-  let shapeVal = JSON.parse(inputFeature[config.gcloud.geometry])
-  //remove geometry from attributes
+  const shapeVal = JSON.parse(inputFeature[config.gcloud.geometry])
+  // remove geometry from attributes
   delete inputFeature[config.gcloud.geometry]
-  //still need to process BigQuery dates
+  // still need to process BigQuery dates
   const feature = {
     type: 'Feature',
     properties: inputFeature,
@@ -101,29 +98,27 @@ function formatFeature(inputFeature) {
   return feature
 }
 
-//Function to determin the name of the spatial column for a table.
-//Queries information schema. If the table has multiple spatial columns, Koop layer parameter is used to determine column.
-async function getSpatialColumn(dataset, table, layerNum){
+// Function to determin the name of the spatial column for a table.
+// Queries information schema. If the table has multiple spatial columns, Koop layer parameter is used to determine column.
+async function getSpatialColumn (dataset, table, layerNum) {
   const query = `SELECT column_name FROM ${dataset}.INFORMATION_SCHEMA.COLUMNS where data_type = 'GEOGRAPHY' and table_name = '${table}';`
   const options = {
     query: query,
     // Location must match that of the dataset(s) referenced in the query.
-    location: 'US',
-  };
-  const [job] = await bigquery.createQueryJob(options);
-  console.log(`Job ${job.id}started.`);
+    location: 'US'
+  }
+  const [job] = await bigquery.createQueryJob(options)
+  console.log(`Job ${job.id}started.`)
 
   // Wait for the query to finish
-  const [rows] = await job.getQueryResults();
-  if (layerNum > (rows.length - 1)) return "-999"
+  const [rows] = await job.getQueryResults()
+  if (layerNum > (rows.length - 1)) return '-999'
   console.log(rows)
   if (rows.length > 0) {
     return rows[layerNum].column_name
+  } else {
+    return null
   }
-  else{
-    return null;
-  }
-
 }
 
 module.exports = Model
